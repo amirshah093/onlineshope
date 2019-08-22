2b382ea50451ace38a04845a9b6174c8cdec9e0b
amirshah srawzewal <amirshah.093@gmail.com>
2019-08-22 18:11:29 +0430

    new file has been add

---
+++ src/homepage.com.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import './homPage.style.scss';
+
+const HomePage = () =>(
+    <div className="hompage">
+        <div className="directory">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Hats </h1>
+                    <span className="subTitle"> Shop Nwo </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Jacket </h1>
+                    <span className="subTitle"> Shop Nwo </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Woments </h1>
+                    <span className="subTitle"> Shop Nwo </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Mants </h1>
+                    <span className="subTitle"> Shop Nwo </span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title"> Shoes </h1>
+                    <span className="subTitle"> Shop Nwo </span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+)
+
+export default HomePage;
