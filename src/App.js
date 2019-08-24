e7266314cc431f4328c3b7cae8991d3c83b35b86
amirshah srawzewal <amirshah.093@gmail.com>
2019-08-24 15:38:50 +0430

    Adding Menu Header components and some styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import HomePage from './pages/hompage/homepage.comp';
 import './App.css';
 import {Route, Switch, } from 'react-router-dom';
 import ShopPage from './pages/Shop/shop-page';
+import Header from './components/header'
+
 
 
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact  path='/' component={HomePage}/>
       <Route exact  path='/shop' component={ShopPage}/>
